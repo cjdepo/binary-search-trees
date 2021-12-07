074bbcf26cfd118a61cad69194e29becc609264d
cjdepo <cjdepowell@protonmail.com>
2021-12-07 14:11:58 -0500

    delete working, not pretty

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -1,5 +1,3 @@
-module Comparable
-end
 
 class Node
 
@@ -31,21 +29,23 @@ class Tree
         @root = root
     end
 
-    def find(value, start_node=@root)
+    def find(value, start_node=@root, parent_node=nil)
         node = start_node
         if node.value == value
-            return node
+            return node, parent_node
         elsif value < node.value
             if node.left == nil
-                return "Not found!"
+                return nil
             else
-                find(value, node.left)
+                parent_node = node
+                find(value, node.left, parent_node)
             end
         elsif value > node.value
             if node.right == nil
-                return "Not found!"
+                return nil
             else
-                find(value, node.right)
+                parent_node = node
+                find(value, node.right, parent_node)
             end
         end
     end
@@ -53,8 +53,7 @@ class Tree
 
     
 
-    def insert(value, start_node=@root)
-        node = start_node
+    def insert(value, node=@root)
         if value == node.value
             return "No duplicate values allowed."
         elsif value < node.value
@@ -71,27 +70,66 @@ class Tree
             end
         end
     end
+    
 
     def delete(value, start_node=@root)
-        node = start_node
-        if value == node.value
-            if !node.left && !node.right
-                # replace reference from parent node with nil
-            elsif node.left
-                # replace reference from parent node with node.left
-            elsif node.right
-                # replace reference from parent node with node.right
-            end
-        elsif value < node.value
-        elsif value > node.value
         
-        end
+        nodes = self.find(value, start_node)
+        node = nodes[0]
+        parent_node = nodes[1]
+        if !parent_node
+            @root = nil
+        else
+            case
+            when !node.left && !node.right
+                case
+                when parent_node.left == node
+                    parent_node.left = nil
+                when parent_node.right == node
+                    parent_node.right = nil
+                end
+            when node.left && !node.right
+                case
+                when parent_node.left == node
+                    parent_node.left = node.left
+                when parent_node.right == node
+                    parent_node.right = node.left
+                end
+            when node.right && !node.left
+                case
+                when parent_node.left == node
+                    parent_node.left = node.right
+                when parent_node.right == node
+                    parent_node.right = node.right
+                end
+            when node.left && node.right
+                inorder_sub = node.right
+                while inorder_sub.left
+                    inorder_parent = inorder_sub
+                    inorder_sub = inorder_sub.left
+                end
+                inorder_parent.left = nil
+                inorder_sub.left = node.left
+                inorder_sub.right = node.right
+                case
+                when parent_node.left == node
+                    parent_node.left = inorder_sub
+                when parent_node.right == node
+                    parent_node.right = inorder_sub
+                end
+            end
+        end   
     end
 end
 
 tree = Tree.new
 tree.build_tree([1, 5, 8, 6, 10, 7, 2])
+p tree
 p tree.find(10)
 p tree.insert(11)
 p tree.insert(11)
+p tree.insert(9)
+p tree.delete(9)
+p tree.delete(5)
+p tree.delete(2)
 p tree.root
